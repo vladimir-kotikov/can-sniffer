b11cfb418221d2dda5c7012089b3938b71198759
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2017-08-28 01:19:51 +0300

    WIP

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 
 import * as Serial from "serialport";
 
-const serial = new Serial("COM3", {
+const serial = new Serial("/dev/cu.SLAB_USBtoUART", {
     baudRate: 57600,
     autoOpen: true
 });
@@ -9,10 +9,76 @@ const serial = new Serial("COM3", {
 serial.on("open", () => console.log("Port is opened"));
 
 serial.on("data", (data: Buffer) => {
-    const len = data.readInt8(1);
-    const arbID = data.readInt32BE(2);
-    const message = data.subarray(4, 4 + len);
-    console.log(data.toString("hex"));
+    const message = CanMessage.fromRawParcel(data);
+    let str = `ArbID: ${message.arbitrationId || "---"} EcuID: ${message.ecuId} : ${message.dataAsHexString()}`;
+    if (message.extended) {
+        str += " EXT: 1"
+    }
+
+    if (message.rtr) {
+        str += " RTR: 1"
+    }
+
+    console.log(str);
 });
 
 serial.on("error", err => console.error(err));
+
+
+class CanMessage {
+    public extended: boolean = false;
+    public rtr: boolean = false;
+    public arbitrationId: number;
+    public ecuId: number;
+    public length: number = 0;
+    public data: number[] = [];
+
+    public static fromRawParcel(parcel: Buffer): CanMessage {
+        let result = new CanMessage();
+        result.length = parcel.readInt8(0);
+
+        const canExtByte = parcel.readInt8(1);
+        result.extended = readBit(7, canExtByte) === 1;
+        result.rtr = readBit(7, canExtByte) === 1;
+
+        const header = (parcel.readInt16LE(2) << 16) & parcel.readInt16LE(4);
+        result.ecuId = readBits(0, 11, header);
+        result.arbitrationId = result.extended ? readBits(12, 29, header) : 0;
+
+        for (var i = 0; i < result.length; i++) {
+            result.data[i] = parcel.readInt8(6 + i);
+        }
+
+        return result;
+    }
+
+    public dataAsHexString(): string {
+        return this.data
+            .map(byte => byte.toString(16))
+            .map(repr => padLeft(repr, 2, "0"))
+            .join(" ");
+    }
+}
+
+export function padLeft(str: string, expectedWidth: number, char: string = " ") {
+    let result = str;
+    while (result.length < expectedWidth) {
+        result = char + result;
+    }
+
+    return result;
+}
+
+export function readBit(bitNo: number, value: number) {
+    return readBits(bitNo, bitNo, value);
+}
+
+export function readBits(startBit: number, endBit: number, value: number) {
+    let mask = 0;
+    for (let i = startBit; i <= endBit; i++)
+        mask |= 1 << i;
+
+    return (value & mask) >> startBit;
+}
+
+
---
+++ test/bitops.test.ts
@@ -0,0 +1,24 @@
+import * as assert from "assert";
+import { readBit, readBits } from "../index";
+
+describe("Binary operations", () => {
+    it("Should be able to take first bit of number", () => {
+        assert.equal(readBits(0, 0, 0b1), 0b1);
+    });
+
+    it("Should be able to take arbitrary bit of number", () => {
+        assert.equal(readBits(1, 1, 0b10), 0b1);
+        assert.equal(readBits(13, 13, 0xc000), 0);
+        assert.equal(readBits(14, 14, 0xc000), 1);
+        assert.equal(readBits(15, 15, 0xc000), 1);
+        assert.equal(readBit(1, 0b10), 0b1);
+    });
+
+    it("Should be able to read a sequence of bits", () => {
+        assert.equal(readBits(0, 1, 0b110), 0b10);
+        assert.equal(readBits(4, 7, 0b00000110), 0);
+    });
+});
+
+
+
