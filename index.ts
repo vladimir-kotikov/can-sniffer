dc8d95de87bf8e2fd8ee11b072e3f642ec976ffa
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2017-08-29 00:06:36 +0300

    Add filtering

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 
+import * as fs from "fs";
+import * as path from "path";
 import * as Serial from "serialport";
 import { CanMessage } from "./src/CanMessage";
+import * as stripComments from "strip-json-comments";
 
 const DEFAULT_PORT: { [platform: string]: string } = {
     win32: "COM3",
@@ -8,7 +11,21 @@ const DEFAULT_PORT: { [platform: string]: string } = {
     linux: "/dev/ttyUSB0",
 };
 
+let FILTERS: number[] = [];
 const DEBUG = process.argv.indexOf("--debug") >= 2;
+const FILTER_FILE = path.resolve("./.canfilters");
+
+if (fs.existsSync(FILTER_FILE)) {
+    loadFilters(FILTERS, FILTER_FILE);
+
+    fs.watch(FILTER_FILE, "utf8", () => {
+        loadFilters(FILTERS, FILTER_FILE);
+    });
+
+    console.log(`Watching for filters from ${FILTER_FILE}`);
+} else {
+    console.log(`Can't find filters in ${FILTER_FILE}. Create one if you want to filter can messages`);
+}
 
 const serial = new Serial(DEFAULT_PORT[process.platform], {
     baudRate: 57600,
@@ -25,9 +42,11 @@ serial.on("data", (data: Buffer) => {
     }
 
     const message = CanMessage.fromRawParcel(data);
-    let str = `ArbID: ${message.arbitrationId ? message.arbitrationId.toString(16) : "---"} ` +
-        `EcuID: ${message.ecuId.toString(16)} : ` +
-        `${message.dataAsHexString()}`;
+    if (FILTERS.indexOf(message.ecuId) >= 0) {
+        return;
+    }
+
+    let str = `EcuID: ${message.ecuId.toString(16)} : ${message.dataAsHexString()}`;
 
     if (message.extended) {
         str += " EXT: 1"
@@ -39,3 +58,22 @@ serial.on("data", (data: Buffer) => {
 
     console.log(str);
 });
+
+function loadFilters(filters: number[], filterFile: string): void {
+    let content: any[];
+    try {
+        content = JSON.parse(stripComments(fs.readFileSync(filterFile, "utf8")));
+        if (!Array.isArray(content)) {
+            return;
+        }
+
+        content = content
+            .map(parseInt)
+            .filter(int => !isNaN(int));
+
+        filters = content;
+        console.log(`Loaded ${filters.length} filters`);
+    } catch (err) {
+        return;
+    }
+}
